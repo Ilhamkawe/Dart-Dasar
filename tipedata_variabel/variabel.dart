2bb4848ac06843f32025ed38882765f83cf3e8e5
ilhamKawe <kawekaweha123@gmail.com>
2020-12-29 07:43:44 +0700

    pertama

---
+++ tipedata_variabel/variabel.dart
@@ -0,0 +1,10 @@
+main() {
+  var nama = "Muhammad Ilham Kusumawardhana";
+  String jurusan = "Informatika";
+  int umur = 20;
+  double tinggi = 175.0;
+  bool jomblo = true;
+
+  print(
+      "Nama Saya $nama , usia saya $umur tahun, tinggi saya $tinggi, dan jomblo ? $jomblo");
+}
